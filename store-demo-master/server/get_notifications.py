68a36376ac1fdb273b519c23659d6e363d8f2b9f
kotosusl <kotosusl@gmail.com>
2024-10-18 15:20:48 +0700

    update notifications

--- store-demo-master/server/get_notifications.py
+++ store-demo-master/server/get_notifications.py
@@ -14,7 +14,7 @@ def get_notifications(current_user):
     session = db_session.create_session()
     query = select(notifications.Notification).select_from(notifications.Notification).where(notifications.Notification.user_id == current_user.id)
     res = session.execute(query)
-    res.sort(key=lambda x: (datetime.strptime(x[0].message_date, '%Y-%m-%d'), datetime.strptime(str(x[0].message_time), '%H:%M:%S')), reverse=True)
+
     jsn = {
         'success': 'OK',
         'notifications': [
